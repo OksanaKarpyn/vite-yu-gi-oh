7edc354c39627649dd1d3ace8163dfc5244093e9
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-05-05 15:03:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+import { store } from './store';
+import HeaderComp from './components/HeaderComp.vue';
+export default {
+  name: 'AppVue',
+  data() {
+    return {
+      store,
+    }
+  },
+  components: {
+    HeaderComp,
+  }
+}
+
+</script>
+
+<template>
+  <HeaderComp></HeaderComp>
+</template>
+
+<style lang="scss">
+@use '../src/style/main.scss';
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'HeaderComp',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+<template></template>
+<style lang="scss" scoped></style>
